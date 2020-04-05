428f13c62a678fa79900b2ec74f89a0687731584
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 02:45:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,36 @@
+# def key_for_min_value(name_hash)
+#   index = 0
+#   lowest_hash = {}
+#
+#   name_hash.collect do |key, value|
+#     if value[index] < value[index + 1]
+#       lowest = value
+#       index = index + 1
+#       key_for_min_value[value]
+#
+#       return lowest
+#     end
+#   end
+# end
+
 def key_for_min_value(name_hash)
-  index = 0
-  lowest_hash = {}
+  # Convert hash to array
+  name_a = name_hash.to_a
 
-  name_hash.collect do |key, value|
-    if value[index] < value[index + 1]
-      lowest = value
-      index = index + 1
-      key_for_min_value[value]
+  # Default key value
+  d_value= 1000
+  d_key= 0
 
-      return lowest
-    end
-  end
-end
+  # Iterate new array
+  name_a.each do |i|
+      # If current value is lower than default, change value&key
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
+
+end  
