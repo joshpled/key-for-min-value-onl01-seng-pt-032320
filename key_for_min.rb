639b38b934401c696e7b7cfa2824aa9751e7d8fd
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 02:34:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  price = 0
+  name_hash.each_value do |key|
+    i = 0
+    if key[i] < key[i+1]
+      price = i
+    else
+      i+1
+    end
+  end
+  i
 end
