766bbf333b61428b83c420c8b2d9e168a644d9fc
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 03:00:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,11 @@
 def key_for_min_value(name_hash)
-  smallest_value = 100000
+  smallest_value = 1000000000
   smallest_key = 0
   name_hash.each do |key, value|
     if value < smallest_value
       smallest_value = value
       smallest_key = key
     end
-
   end
   smallest_key
 end
