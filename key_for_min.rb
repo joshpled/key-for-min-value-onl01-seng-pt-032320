800645041dc421157e144bf840247bd42cf11ffe
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 02:48:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -39,11 +39,11 @@ end
 # def key_for_min_value(name_hash)
 #   # Convert hash to array
 #   name_a = name_hash.to_a
-# 
+#
 #   # Default key value
 #   d_value= 1000
 #   d_key= 0
-# 
+#
 #   # Iterate new array
 #   name_a.each do |i|
 #       # If current value is lower than default, change value&key
@@ -51,9 +51,9 @@ end
 #         d_value = i[1]
 #         d_key = i[0]
 #       end
-# 
+#
 #    end
-# 
+#
 #   return d_key
-# 
+#
 # end
